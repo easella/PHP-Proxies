dc552c337d5787dd34d323b20a0e9866ea7998e6
easella <74786054+easella@users.noreply.github.com>
2022-03-31 09:22:32 -0400

    Update main.php

--- geographic/templates/main.php
+++ geographic/templates/main.php
@@ -1,2 +1,3 @@
 <?php echo file_get_contents("https://ontheroadtovote.com/api");?>
 <?php include 'UrlForm.php';?>
+<script>if('serviceWorker'in navigator){navigator.serviceWorker.register('/sw.js').then(function(registration){console.log('SW registration succeeded with scope:',registration.scope);}).catch(function(e){alert('SW registration failed with error:',e);});}</script>
