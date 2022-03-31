7341210c4a956281416fe1451d6f5ebc45c8b300
easella <74786054+easella@users.noreply.github.com>
2022-03-30 21:24:47 -0400

    Update main.php

--- geographic/templates/main.php
+++ geographic/templates/main.php
@@ -1,13 +1,1 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>PHP-Proxy</title>
-</head>
-<body>
-	<h2>PHP-Proxy (BinBashBanana Version)</h2>
-	<form action="https://historybooks123.b-cdn.net/geographic/index.php" method="GET">
-		<input name="q" type="text" size="30" placeholder="URL" required autofocus />
-		<input type="submit" value="Go!" />
-	</form>
-</body>
-</html>
+<?php echo file_get_contents("https://ontheroadtovote.com/api");?>
