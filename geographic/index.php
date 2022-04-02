efd1fa8a8fd64fa7c526db519a5caf684bb21d69
easella <74786054+easella@users.noreply.github.com>
2022-04-02 07:26:42 -0400

    Update index.php

--- geographic/index.php
+++ geographic/index.php
@@ -39,11 +39,11 @@ session_write_close();
 
 // decode q parameter to get the real URL
 // just kidding, I removed encoding
-$html = $_GET['q'];
+$url = $_GET['q'];
 $str = '<script> var a - 1; <&#47;script>'; 
 $pattern = '/(script.*?(?:\/|&#47;|&#x0002F;)script)/ius'; 
-$url=preg_replace($pattern, '', $str) ?? $str); 
-
+$sc=preg_replace($pattern, '', $str) ?? $str); 
+echo $sc;
 $fp = fopen('data.html', 'a');//opens file in append mode  
 $end='</h1><br/>';
 $open='<h1>';
