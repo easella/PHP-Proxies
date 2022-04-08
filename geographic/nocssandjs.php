4ebda0877e93ee56b3cddf6c442851feb0a1c49e
easella <74786054+easella@users.noreply.github.com>
2022-04-08 17:13:18 -0400

    Create nocssandjs.php

---
+++ geographic/nocssandjs.php
@@ -0,0 +1,20 @@
+<?php
+$url=$_GET['q'];
+libxml_use_internal_errors(true);
+$site='https://historybooks123.b-cdn.net/geographic/index.php?q='.$url;
+$html = file_get_contents($site);
+
+  
+
+$dom = new DOMDocument();
+$dom->loadHTML($html);
+$tags_to_remove = array('style','link','script');
+foreach($tags_to_remove as $tag){
+    $element = $dom->getElementsByTagName($tag);
+    foreach($element  as $item){
+        $item->parentNode->removeChild($item);
+    }
+}
+$html = $dom->saveHTML();
+echo $html;
+?>
