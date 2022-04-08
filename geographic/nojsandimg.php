b6345085c893909af095feb03da6eb8647fb564d
easella <74786054+easella@users.noreply.github.com>
2022-04-08 17:38:31 -0400

    Create nojsandimg.php

---
+++ geographic/nojsandimg.php
@@ -0,0 +1,33 @@
+<?php
+$url=$_GET['q'];
+libxml_use_internal_errors(true);
+$site='https://historybooks123.b-cdn.net/geographic/index.php?q='.$url;
+$html = file_get_contents($site);
+
+  
+
+$dom = new DOMDocument();
+
+$dom->loadHTML($html);
+
+$script = $dom->getElementsByTagName('script');
+
+$img = $dom->getElementsByTagName('img');
+$remove = [];
+foreach($script as $item)
+{
+  $remove[] = $item;
+}
+
+foreach($img as $item)
+{
+  $remove[] = $item;
+}
+foreach ($remove as $item)
+{
+  $item->parentNode->removeChild($item); 
+}
+
+$html = $dom->saveHTML();
+echo $html;
+?>
