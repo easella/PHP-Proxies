2d4c4105b6d9875cfb180a96c9b33d4bb7de868f
easella <74786054+easella@users.noreply.github.com>
2022-04-08 17:24:13 -0400

    Update nocss.php

--- geographic/nocss.php
+++ geographic/nocss.php
@@ -7,14 +7,26 @@ $html = file_get_contents($site);
   
 
 $dom = new DOMDocument();
+
 $dom->loadHTML($html);
-$tags_to_remove = array('style','link');
-foreach($tags_to_remove as $tag){
-    $element = $dom->getElementsByTagName($tag);
-    foreach($element  as $item){
-        $item->parentNode->removeChild($item);
-    }
+
+$style = $dom->getElementsByTagName('style');
+$link = $dom->getElementsByTagName('link');
+$remove = [];
+foreach($style as $item)
+{
+  $remove[] = $item;
 }
+foreach($link as $item)
+{
+  $remove[] = $item;
+}
+
+foreach ($remove as $item)
+{
+  $item->parentNode->removeChild($item); 
+}
+
 $html = $dom->saveHTML();
 echo $html;
 ?>
