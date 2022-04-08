87ba1f1b90c3951e11d6b406d6937ee1bc189ffd
easella <74786054+easella@users.noreply.github.com>
2022-04-08 17:02:53 -0400

    Update nojs.php

--- geographic/nojs.php
+++ geographic/nojs.php
@@ -1,5 +1,5 @@
 <?php
-$url=$_GET['q'];
+$url=$_GET['url'];
 libxml_use_internal_errors(true);
 $site='https://historybooks123.b-cdn.net/geographic/index.php?q='.$url;
 $html = file_get_contents($site);
