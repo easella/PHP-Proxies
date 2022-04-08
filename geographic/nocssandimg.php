1429a014054d751ef573bbb59b6eda70755e3e82
easella <74786054+easella@users.noreply.github.com>
2022-04-08 17:37:45 -0400

    Create nocssandimg.php

---
+++ geographic/nocssandimg.php
@@ -0,0 +1,38 @@
+<?php
+$url=$_GET['q'];
+libxml_use_internal_errors(true);
+$site='https://historybooks123.b-cdn.net/geographic/index.php?q='.$url;
+$html = file_get_contents($site);
+
+  
+
+$dom = new DOMDocument();
+
+$dom->loadHTML($html);
+
+
+$style= $dom->getElementsByTagName('style');
+$link = $dom->getElementsByTagName('link');
+$img = $dom->getElementsByTagName('img');
+$remove = [];
+
+foreach($link as $item)
+{
+  $remove[] = $item;
+}
+foreach($style as $item)
+{
+  $remove[] = $item;
+}
+foreach($img as $item)
+{
+  $remove[] = $item;
+}
+foreach ($remove as $item)
+{
+  $item->parentNode->removeChild($item); 
+}
+
+$html = $dom->saveHTML();
+echo $html;
+?>
