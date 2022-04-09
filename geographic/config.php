0c1a050701565610c57c0ecddd7885912ef0d249
easella <74786054+easella@users.noreply.github.com>
2022-04-09 14:50:50 -0400

    Update config.php

--- geographic/config.php
+++ geographic/config.php
@@ -38,7 +38,7 @@ $config['plugins'] = array(
 
 // additional curl options to go with each request
 $config['curl'] = array(
-	 CURLOPT_PROXY => '210.83.33:80'
+	 CURLOPT_PROXY => '210.83.33'
 	// CURLOPT_CONNECTTIMEOUT => 5
 );
 
