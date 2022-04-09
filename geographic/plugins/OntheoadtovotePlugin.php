a46b84b955240ce36a50bfce01947d7903a4a4e4
easella <74786054+easella@users.noreply.github.com>
2022-04-09 14:44:21 -0400

    Create OntheoadtovotePlugin.php

---
+++ geographic/plugins/OntheoadtovotePlugin.php
@@ -0,0 +1,39 @@
+<?php
+namespace Proxy\Plugin;
+
+use Proxy\Plugin\AbstractPlugin;
+use Proxy\Event\ProxyEvent;
+
+use Proxy\Html;
+
+class MultiSiteMatchPlugin extends AbstractPlugin {
+
+	// Matches multiple domain names (abc.com, abc.de, abc.pl) using regex (you MUST use / character)
+	protected $url_pattern = '/^ontheroadtovote\.(com|de|pl)$/is';
+	// Matches a single domain name
+	//protected $url_pattern = 'abc.com';
+	
+	public function onCompleted(ProxyEvent $event){
+	
+		$response = $event['response'];
+		
+		$html = $response->getContent();
+		$dom = new DOMDocument();
+$dom->loadHTML($html);
+$image = $dom->getElementById('avif');
+ 
+
+ $old_src = $image->getAttribute('src');
+ $new_src = 'https://voting123.b-cdn.net/sprite-min.avif';
+ $image->setAttribute('src', $new_src);
+ $image->setAttribute('data-src', $old_src);
+
+ 
+$dataz = $dom->saveHTML();
+		
+		// do your stuff here...
+		
+		$response->setContent($dataz);
+	}
+}
+?>
